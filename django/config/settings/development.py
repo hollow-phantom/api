7c28fcf29f016ee11eda406677bd3c97a5cc7083
hollow-phantom <hollow.phantom728@gmail.com>
2026-03-11 17:47:15 +0900

    initial commit

---
+++ django/config/settings/development.py
@@ -0,0 +1,37 @@
+from .base import *
+
+DEBUG = True
+
+# 開発時は全ホストからのアクセスを許可
+ALLOWED_HOSTS = ["*"]
+
+# 開発用ツール
+INSTALLED_APPS += [
+    "debug_toolbar",
+]
+
+MIDDLEWARE += [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+]
+
+INTERNAL_IPS = ["127.0.0.1"]
+
+# 開発時はコンソールにメール出力
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+# SQLをコンソールに出力（クエリ確認用）
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "loggers": {
+        "django.db.backends": {
+            "handlers": ["console"],
+            "level": "DEBUG",
+        },
+    },
+}
---
+++ django/config/settings/production.py
@@ -0,0 +1,32 @@
+from .base import *
+
+DEBUG = False
+
+# セキュリティ設定
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+X_FRAME_OPTIONS = "DENY"
+
+# 本番用ロギング
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "{levelname} {asctime} {module} {message}",
+            "style": "{",
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "verbose",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": "WARNING",  # WARNING以上だけ出力
+        },
+    },
+}
